fd5375d11a3d44324f9e8886a05f6bcfc5fc2a80
Mohamed Sherif <m.sherif9971@gmail.com>
2022-12-13 14:50:28 +0200

    first

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ random.seed(43)
 np.set_printoptions(threshold=sys.maxsize)
 
 
-def main(args):
+def main1(args):
 
     if not os.path.exists('results'):
         os.makedirs('results')
@@ -30,12 +30,15 @@ def main(args):
             os.makedirs('results/{}/recs'.format(dataset))
 
         df = pd.read_csv('datasets/{}_trainingset.tsv'.format(dataset), sep='\t', names=['user_id', 'item_id', 'rating'])
+        print(df)
         df, reverse_dict = utils.convert_unique_idx(df, 'item_id')
         user_size = len(df['user_id'].unique())
         item_size = len(df['item_id'].unique())
         print('Found {} users and {} items'.format(user_size, item_size))
         train_user_lists = utils.create_user_lists(df, user_size, 3)
         train_interactions_size = sum([len(user_list) for user_list in train_user_lists])
+        print(train_user_lists[1])
+        # print("train interaction size",train_interactions_size)
         print('{} interactions considered for training'.format(train_interactions_size))
 
         # Set parameters based on arguments
@@ -81,16 +84,15 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datasets', nargs='+', help='Set the datasets you want to use', required=True)
-    parser.add_argument('-F', '--n_factors', nargs='+', help='Set the latent factors you want', type=int, required=True)
+    parser.add_argument('--datasets', nargs='+', help='Set the datasets you want to use',default=['AmazonDigitalMusic'])
+    parser.add_argument('-F', '--n_factors', nargs='+', help='Set the latent factors you want', type=int, default=[5])
     parser.add_argument('-pi', '--positive_fraction', type=float, help='Set the fraction of positive item to send (default 0)')
-    parser.add_argument('-U', '--fraction', help='Set the fraction of clients per round (0 for just one client)', type=float, default=0, required=True)
-    parser.add_argument('-T', '--sampler_size', help='Set the sampler size: single for 1, uniform for R/U')
-    parser.add_argument('-lr', '--lr', nargs='+', help='Set the learning rates', type=float, required=True)
-    parser.add_argument('-E', '--n_epochs', help='Set the number of epochs', type=int, required=True)
+    parser.add_argument('-U', '--fraction', help='Set the fraction of clients per round (0 for just one client)', type=float, default=0.3)
+    parser.add_argument('-T', '--sampler_size', help='Set the sampler size: single for 1, uniform for R/U',default='single')
+    parser.add_argument('-lr', '--lr', nargs='+', help='Set the learning rates', type=float, default=[0.05])
+    parser.add_argument('-E', '--n_epochs', help='Set the number of epochs', type=int, default=5)
     parser.add_argument('--with_delta', action='store_true', help='Use if you want server to send deltas instead of overwriting item information')
     parser.add_argument('--validation_size', help='Set a validation size, if needed', type=float, default=0)
-    parser.add_argument('--eval_every', type=int, default=10)
+    parser.add_argument('--eval_every', type=int, default=1)
     parser.add_argument('--mp', action='store_true', help='Use if you want to use multiprocessing (if fraction > 0)')
-    parsed_args = parser.parse_args()
-    main(parsed_args)
+    main1(parser.parse_args())
